c39f4d4fee5c08f334aa4e862892edcd193b9292
carmencitaqiu <wendyjisung@aliyun.com>
2024-02-16 10:17:30 +0800

    fix:error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import { SettingProvider } from '../context/SettingContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SettingProvider>
       <Component {...pageProps} />
-    </SettingProvider>
   )
 }
 
