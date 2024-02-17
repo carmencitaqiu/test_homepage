c9b0f90b0a6f4eb6744c1b28666a9c1fca4757a8
carmencitaqiu <wendyjisung@aliyun.com>
2024-02-17 17:17:25 +0800

    fix:products

--- components/index/Products.tsx
+++ components/index/Products.tsx
@@ -4,6 +4,19 @@ import { Navigation} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 export default function Products() {
+    const arr = [{
+        title: 'Filtration moléculaire',
+        imgSrc: 'product1.png',
+    },{
+        title: 'Filtration CVAC',
+        imgSrc: 'product2.png',
+    },{
+        title: 'Filtre  à haute pureté',
+        imgSrc: 'product3.png',
+    },{
+        title: 'Purificateurs d’air',
+        imgSrc: 'product4.png',
+    }]
     return (
         <div className="w-full">
             <div className="mx-auto container mt-36">
@@ -20,18 +33,18 @@ export default function Products() {
                 className="!pt-12"
                 >
                     {
-                        new Array(10).fill("").map((item, index) => (
+                        arr.map((item, index) => (
                             <SwiperSlide className="!overflow-visible " key={index}>
                                 <div>
                                     <div className="w-full h-[344px]  bg-white-1 rounded-3xl relative !overflow-visible">
-                                        <div className="absolute z-10 -top-16 left-1/2 w-96 h-96 bg-contain -translate-x-1/2" style={{backgroundImage: 'url("/static/images/product1.png")'}}>
+                                        <div className="absolute z-10 -top-16 left-1/2 w-96 h-96 bg-contain -translate-x-1/2" style={{backgroundImage: `url("/static/images/${item.imgSrc}")`}}>
                                         </div>
-                                        <div className="absolute bottom-6 left-4  font-medium text-left">Filtration moléculaire</div>
+                                        <div className="absolute bottom-6 left-4  font-medium text-left">{item.title}</div>
                                     </div>
-                                    <div className="w-3/4 bg-green-1 flex items-center justify-between mt-9 space-x-5 py-2 cursor-pointer pl-9 pr-5">
-                                        <div className="uppercase text-white text-xl font-medium ">MAGASINEZ</div>
+                                    <div className="w-1/2 bg-green-1 flex items-center justify-between mt-6 space-x-5 py-2 cursor-pointer px-3">
+                                        <div className="uppercase text-white text-sm 2xl:text-base">MAGASINEZ</div>
                                         <div className="mt-2">
-                                            <Image src="/static/icons/right_arrow.svg" width={30} height={30} alt="right_arrow" />
+                                            <Image src="/static/icons/right_arrow.svg" width={24} height={24} alt="right_arrow" />
                                         </div>
                                     </div>
                                 </div>
